3e237b4af84489675c9eee505f55594f4dbea0e7
Rafael Acurcio <rafael.acurcio@gmail.com>
2018-01-18 18:44:34 -0300

    Barebones of pygame window

---
+++ 129/app.py
@@ -0,0 +1,25 @@
+import pygame
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+
+pygame.display.set_caption("Missile Command")
+
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+	for event in pygame.event.get():
+		if(event.type == pygame.QUIT):
+			crashed = True
+		print(event)
+	pygame.display.update()
+
+	clock.tick(30)
+
+pygame.quit()
+quit()
